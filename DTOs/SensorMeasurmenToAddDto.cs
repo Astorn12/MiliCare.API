be02b2b4fd5ab07248fd58fa11c869f7786a105b
unknown <oskaz.zadora@gmail.com>
2020-04-06 21:33:14 +0200

    Small changes

--- DTOs/SensorMeasurmenToAddDto.cs
+++ DTOs/SensorMeasurmenToAddDto.cs
@@ -2,7 +2,6 @@ namespace MiliCare.DTOs
 {
     public class SensorMeasurmenToAddDto
     {
-         public int Id { get; set; }
         public int UserId { get; set; }
         public int SensorId { get; set; }
     }
